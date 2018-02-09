3763c02f64752b9f9d8c24ec408e2f940b94c1b7
Daljeet Singh <daljeet.s@vcloudx.com>
2018-02-09 18:35:11 +0530

    Second Commit 0.1.2

--- MorePodRun.podspec
+++ MorePodRun.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'MorePodRun'
-  s.version          = '0.1.1'
+  s.version          = '0.1.2'
   s.summary          = 'My first MorePodRun testing on it at my house.'
 
 # This description is used to generate tags and improve search results.
---
+++ MorePodRun/Classes/SamplePodFramework.framework/Headers/SamplePodFramework.h
@@ -0,0 +1,19 @@
+//
+//  SamplePodFramework.h
+//  SamplePodFramework
+//
+//  Created by Daljeet Singh on 09/02/18.
+//  Copyright © 2018 Daljeet Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SamplePodFramework.
+FOUNDATION_EXPORT double SamplePodFrameworkVersionNumber;
+
+//! Project version string for SamplePodFramework.
+FOUNDATION_EXPORT const unsigned char SamplePodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamplePodFramework/PublicHeader.h>
+
+
